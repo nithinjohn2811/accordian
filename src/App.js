01b215aadd09eb949ee36d862506fac845938f60
Nithin John Cherian <Nithinjohn2811"gmail.com>
2024-02-05 20:16:02 +0000

    single selection complete

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import { useState } from "react";
 import data from "./data";
 
 function App() {
+  const [selected, setselected] = useState(null);
+  const [multiselect, setmultiselect] = useState(null)
 
-const [selected, setSelected] = useState(null)
+  function handleSingleSelection(getCurrentId) {
+    setselected(getCurrentId === selected?null:getCurrentId);
+  }
+
+  function handleMultipleSelection(getMultipleIds){
+
+  }
   return (
     <div className="wrapper">
-    <div>
-      {data && data.length > 0?
-       data.map((dataItem)=>
-       <div className="item">
-        <h3 className="title">{dataItem.question}</h3>
-       </div>): 
-      <div>There is no data</div>}
-    </div>
+      <div>
+        <div>
+          <button>multi selection</button>
+        </div>
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>There is no data</div>
+        )}
+      </div>
     </div>
   );
 }
