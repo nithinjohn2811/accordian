38de5709af92f9f98dad1aa35f16faae516a67fc
Nithin John Cherian <Nithinjohn2811"gmail.com>
2024-02-05 19:24:27 +0000

    data map and question

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import data from "./data";
+
 function App() {
+
+const [selected, setSelected] = useState(null)
   return (
-    <div className="App">
-     
+    <div className="wrapper">
+    <div>
+      {data && data.length > 0?
+       data.map((dataItem)=>
+       <div className="item">
+        <h3 className="title">{dataItem.question}</h3>
+       </div>): 
+      <div>There is no data</div>}
+    </div>
     </div>
   );
 }
